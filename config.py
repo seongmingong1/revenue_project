bb3f918d4b97ea7f0a954f920494a2c4150432e1
Seong Min <149540654+seongmingong1@users.noreply.github.com>
2025-08-07 00:19:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from airflow.models import Variable
 
 class Config:
-    FILE_PATH = Variable.get("lesson_data_path", 
-                           default_var="/opt/airflow/dags/lesson_data")
+    FILE_PATH = Variable.get("income_data_path", 
+                           default_var="/opt/airflow/dags/income_data")
     API_TIMEOUT = int(os.getenv('API_TIMEOUT', '10'))
     CSV_OUTPUT_PATH = os.getenv('CSV_OUTPUT_PATH', 
-                               '/opt/airflow/dags/lesson_data/')
+                               '/opt/airflow/dags/income_data/')
     
     @classmethod
     def update_file_path(cls, new_path):
